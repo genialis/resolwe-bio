299528a8117f6a5801c8d57b8b50cff9a19e022c
Janez Kokosar <janez@genialis.com>
2015-02-05 14:18:55 -0600

    Added tests for fastqmcf and prinseq processors.

--- resolwe_bio/tests/test_reads_filtering.py
+++ resolwe_bio/tests/test_reads_filtering.py
@@ -35,3 +35,49 @@ class ReadsFilteringProcessorTestCase(BaseProcessorTestCase):
         self.assertFiles(filtered_reads, 'fastq', 'reads_wo_rRNA_paired_forw.fastq.gz')
         self.assertFiles(filtered_reads, 'fastq2', 'reads_wo_rRNA_paired_rew.fastq.gz')
         self.assertFiles(filtered_reads, 'fastq_rRNA', 'reads_rRNA_paired.fastq.gz')
+
+    def test_prinseq_single(self):
+        inputs = {'src': 'reads.fastq.gz'}
+        reads = self.run_processor('import:upload:reads-fastq', inputs)
+        self.assertDone(reads)
+
+        inputs = {'reads': reads.pk}
+        filtered_reads = self.run_processor('filtering:prinseq-lite-20.4:single-end', inputs)
+        self.assertDone(filtered_reads)
+        self.assertFiles(filtered_reads, 'fastq', 'filtered_reads_prinseq_single.fastq.gz')
+
+    def test_prinseq_paired(self):
+        inputs = {
+            'src1': 'rRNA_forw.fastq.gz',
+            'src2': 'rRNA_rew.fastq.gz'}
+        reads = self.run_processor('import:upload:reads-fastq-paired-end', inputs)
+        self.assertDone(reads)
+
+        inputs = {'reads': reads.pk}
+        filtered_reads = self.run_processor('filtering:prinseq-lite-20.4:paired-end', inputs)
+        self.assertDone(filtered_reads)
+        self.assertFiles(filtered_reads, 'fastq', 'filtered_reads_prinseq_paired_fw.fastq.gz')
+        self.assertFiles(filtered_reads, 'fastq2', 'filtered_reads_prinseq_paired_rw.fastq.gz')
+
+    def test_fastqmcf_single(self):
+        inputs = {'src': 'reads.fastq.gz'}
+        reads = self.run_processor('import:upload:reads-fastq', inputs)
+        self.assertDone(reads)
+
+        inputs = {'reads': reads.pk}
+        filtered_reads = self.run_processor('filtering:fastq-mcf-1.1.2.537:single-end', inputs)
+        self.assertDone(filtered_reads)
+        self.assertFiles(filtered_reads, 'fastq', 'filtered_reads_fastqmcf_single.fastq.gz')
+
+    def test_fastqmcf_paired(self):
+        inputs = {
+            'src1': 'rRNA_forw.fastq.gz',
+            'src2': 'rRNA_rew.fastq.gz'}
+        reads = self.run_processor('import:upload:reads-fastq-paired-end', inputs)
+        self.assertDone(reads)
+
+        inputs = {'reads': reads.pk}
+        filtered_reads = self.run_processor('filtering:fastq-mcf-1.1.2.537:paired-end', inputs)
+        self.assertDone(filtered_reads)
+        self.assertFiles(filtered_reads, 'fastq', 'filtered_reads_fastqmcf_paired_fw.fastq.gz')
+        self.assertFiles(filtered_reads, 'fastq2', 'filtered_reads_fastqmcf_paired_rw.fastq.gz')
