d206790280ae72af0113de43244ceea95893f4cb
Janez Kokosar <janez@genialis.com>
2015-02-25 16:27:44 -0600

    DESeq2 processor test

--- resolwe_bio/tests/test_diff_expression.py
+++ resolwe_bio/tests/test_diff_expression.py
@@ -148,3 +148,61 @@ class DiffExpProcessorTestCase(BaseProcessorTestCase):
         diff_exp = self.run_processor('differentialexpression:bcm-1-0-0', inputs)
         self.assertDone(diff_exp)
         self.assertJSON(diff_exp.output['volcano_plot'], '', 'bayseq_volcano.json')
+
+    def test_deseq2(self):
+        genome = self.prepair_genome()
+
+        inputs = {'src': '00Hr.fastq.gz'}
+        reads1 = self.run_processor('import:upload:reads-fastq', inputs)
+        self.assertDone(reads1)
+
+        inputs = {'src': '20Hr.fastq.gz'}
+        reads2 = self.run_processor('import:upload:reads-fastq', inputs)
+        self.assertDone(reads2)
+
+        inputs = {'src': 'annotation.gtf'}
+        annotation = self.run_processor('import:upload:annotation-gtf', inputs)
+        self.assertDone(annotation)
+        self.assertFiles(annotation, 'gtf', 'annotation.gtf')
+
+        inputs = {
+            'genome': genome.pk,
+            'reads': reads1.pk,
+            'gff': annotation.pk,
+            'PE_options': {
+                'library_type': "fr-unstranded"}}
+        aligned_reads_1 = self.run_processor('alignment:tophat-2-0-13', inputs)
+        self.assertDone(aligned_reads_1)
+
+        inputs = {
+            'genome': genome.pk,
+            'reads': reads2.pk,
+            'gff': annotation.pk,
+            'PE_options': {
+                'library_type': "fr-unstranded"}}
+        aligned_reads_2 = self.run_processor('alignment:tophat-2-0-13', inputs)
+        self.assertDone(aligned_reads_2)
+
+        inputs = {
+            'alignments': aligned_reads_1.pk,
+            'gff': annotation.pk,
+            'stranded': "no",
+            'id_attribute': 'transcript_id'}
+        expression_1 = self.run_processor('htseq-count:-0-6-1p1', inputs)
+        self.assertDone(expression_1)
+
+        inputs = {
+            'alignments': aligned_reads_2.pk,
+            'gff': annotation.pk,
+            'stranded': "no",
+            'id_attribute': 'transcript_id'}
+        expression_2 = self.run_processor('htseq-count:-0-6-1p1', inputs)
+        self.assertDone(expression_2)
+
+        inputs = {
+            'name': "00vs20",
+            'case': [expression_1.pk],
+            'control': [expression_2.pk]}
+        diff_exp = self.run_processor('differentialexpression:deseq2', inputs)
+        self.assertDone(diff_exp)
+        self.assertFiles(diff_exp, "diffexp", 'diffexp_deseq2.tab.gz', gzipped=True)
