2a15415385ff6674fc2a33f00d39f5946ffdd870
Janez Kokosar <janez@genialis.com>
2015-03-20 09:58:40 -0500

    Updated "Gene Info" upload processor

--- resolwe_bio/tools/import_geneinfo.py
+++ resolwe_bio/tools/import_geneinfo.py
@@ -10,28 +10,48 @@ if not os.path.isfile(file_name):
 
 
 def import_excel(file_name):
-    meta = {'geneinfo': []}
+    meta = {'data': [], "header": []}
     workbook = xlrd.open_workbook(file_name)
     worksheet = workbook.sheets()[0]
     num_rows = worksheet.nrows
+    num_cols = worksheet.ncols
+
+    for x in range(0, num_cols):
+        meta["header"].append(str(worksheet.cell_value(0, x)))
+
+    data = {}
+
+    for i, l in enumerate(meta["header"]):
+        data[l] = i
+
     for x in range(1, num_rows):
-        meta['geneinfo'].append({
-            "id": str(worksheet.cell_value(x, 0)),
-            "name": str(worksheet.cell_value(x, 1)),
-            "synonyms": str(worksheet.cell_value(x, 2)),
-            "description": str(worksheet.cell_value(x, 3))})
+        out = {}
+
+        for label in data:
+            out[label] = str(worksheet.cell_value(x, data[label]))
+
+        meta['data'].append(out)
     return meta
 
 
 def import_table(file_name):
-    meta = {'geneinfo': []}
+    meta = {'data': [], 'header': []}
     with open(file_name, 'rU') as csvfile:
         my_reader = csv.reader(csvfile, delimiter='\t')
-        next(my_reader, None)
+        header = next(my_reader, None)
+        meta["header"] = header
+        data = {}
+
+        for i, l in enumerate(header):
+            data[l] = i
+
         for vals in my_reader:
-            meta['geneinfo'].append({"id": vals[0], "name": vals[1],
-                                     "synonyms": vals[2],
-                                     "description": vals[3]})
+            out = {}
+
+            for label in data:
+                out[label] = vals[data[label]]
+
+            meta['data'].append(out)
     return meta
 
 
