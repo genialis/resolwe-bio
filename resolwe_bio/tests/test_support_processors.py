a46164dd6c7fff895fe1b832c9fe57fee938bf76
Janez Kokosar <janez@genialis.com>
2015-10-01 15:20:37 -0500

    Feature location processor
    Improved STAR names

--- resolwe_bio/tests/test_support_processors.py
+++ resolwe_bio/tests/test_support_processors.py
@@ -12,3 +12,14 @@ class CompatibilityProcessorTestCase(BaseProcessorTestCase, PreparedData):
         inputs = {'reference': genome.pk, 'bam': mapping.pk, 'annot': annotation.pk}
         compatibility_test = self.run_processor('reference_compatibility', inputs)
         self.assertFiles(compatibility_test, 'report_file', 'sp_test_compatibility_report.txt')
+
+    def test_feature_location(self):
+        inputs = {'src': 'mm10_small.gtf.gz'}
+        annotation = self.run_processor('import:upload:annotation-gtf', inputs)
+
+        inputs = {'annotation': annotation.pk,
+                  'feature_type': 'exon',
+                  'id_type': 'transcript_id',
+                  'summarize_exons': True}
+        features = self.run_processor('feature_location', inputs)
+        self.assertJSON(features, features.output['feature_location'], '', 'feature_locations.json.gz')
---
+++ resolwe_bio/tools/feature_location.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python2
+import json
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description='Extract feature locations')
+parser.add_argument('--annotation', help='Annotation file')
+parser.add_argument('--feature_type', help='Feature type')
+parser.add_argument('--id_type', help='Feature ID')
+parser.add_argument('--summarize_exons', action='store_true', default=False)
+args = parser.parse_args()
+
+gene_id_re = re.compile('gene_id "([\w\-\.]*)"')
+transcript_id_re = re.compile('transcript_id "([\w\-\.]*)"')
+id_re = re.compile('ID=([\w\-\.]*)')
+parent_re = re.compile('Parent=([\w\-\.]*)')
+
+
+def _search(regex, string):
+    match = regex.search(string)
+    return match.group(1) if match else 'N/A'
+
+
+def get_gene_id(ids):
+    return _search(gene_id_re, ids)
+
+
+def get_transcript_id(ids):
+    return _search(transcript_id_re, ids)
+
+
+def get_id(ids):
+    return _search(id_re, ids)
+
+
+def get_parent_id(ids):
+    return _search(id_re, ids)
+
+
+def fix_invalid_key(key):
+    return key.replace('$', u'\uff04').replace('.', u'\uff0e')
+
+
+feature_locations = {}
+
+with open(args.annotation) as f:
+    n_rows = sum(1 for _ in f)
+
+count, nrows = 0, float(n_rows)
+count_span = nrows / 100.
+count_threshold = count_span
+
+with open(args.annotation) as annotation:
+    for l in annotation:
+        a = l.split('\t')
+        if len(a) < 8:
+            continue
+        if a[2] == args.feature_type:
+            chromosome = a[0]
+            start = a[3]
+            end = a[4]
+
+            if args.id_type == 'ID':
+                feature_id = get_id(l)
+            elif args.id_type == 'Parent':
+                feature_id = get_parent_id(l)
+            elif args.id_type == 'gene_id':
+                feature_id = get_gene_id(l)
+            elif args.id_type == 'transcript_id':
+                feature_id = get_transcript_id(l)
+
+            if args.summarize_exons:
+                if feature_id not in feature_locations:
+                    feature_locations[feature_id] = {'chr': chromosome, 'str': [], 'end': []}
+
+                feature_locations[feature_id]['str'].append(start)
+                feature_locations[feature_id]['end'].append(end)
+
+            else:
+                feature_locations[feature_id] = {'chr': chromosome, 'str': start, 'end': end}
+
+        count += 1
+        if count > count_threshold:
+            print '{{"proc.progress": {}}}'.format(round(count / nrows, 2))
+            count_threshold += count_span
+
+    if args.summarize_exons:
+        for feature_id in feature_locations:
+            feature_locations[feature_id]['str'] = str(min(map(int, feature_locations[feature_id]['str'])))
+            feature_locations[feature_id]['end'] = str(max(map(int, feature_locations[feature_id]['end'])))
+
+new_feature_locations = {fix_invalid_key(key): val for (key, val) in feature_locations.iteritems()}
+print json.dumps({'feature_location': new_feature_locations}, separators=(',', ':'))
