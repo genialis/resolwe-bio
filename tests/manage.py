1f12e2bbcaada4e2f39e24e25f7a205c1eefec85
Domen Blenkuš <domen@blenkus.com>
2016-01-14 11:54:03 +0100

    Prepare minimal project for running tests

---
+++ tests/manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import sys
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, PROJECT_ROOT)
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
---
+++ tests/urls.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from django.conf.urls import include, url
+
+
+urlpatterns = [
+    url(r'^', include('resolwe.api_urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
