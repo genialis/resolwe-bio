505115b1cfcb055961457877c1403e0cdfe939ea
Miha Stajdohar <miha.stajdohar@gmail.com>
2016-01-28 15:22:00 -0600

    Initial docs

---
+++ docs/index.rst
@@ -0,0 +1,29 @@
+
+.. _index:
+
+======================
+Resolwe Bioinformatics
+======================
+
+Write the abstract.
+
+Contents
+========
+
+.. toctree::
+   :maxdepth: 2
+
+   intro
+   proc
+
+Process Catalog
+===============
+
+Automatically import a list of processes from the YML files. Organize them in
+categories.
+
+
+Pipelines Examples
+==================
+
+For example, the RNA-seq pipeline.
--- setup.py
+++ setup.py
@@ -17,38 +17,58 @@ LICENSE = 'Apache License (2.0)'
 if __name__ == '__main__':
     setup(
         name=NAME,
+
         version=VERSION,
+
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
+
+        url=URL,
+
         author=AUTHOR,
         author_email=AUTHOR_EMAIL,
-        url=URL,
+
         license=LICENSE,
+
         packages=['resolwe_bio'],
         include_package_data=True,
+        zip_safe=False,
+        dependency_links=(
+            "git+https://github.com/genialis/resolwe.git@9d6911e5834b5d959f80ec20906813903a35c084#egg=resolwe-0.9",
+        ),
+        install_requires=(
+            "resolwe>=0.9",
+        ),
+        extras_require = {
+            'docs':  ['sphinx>=1.3.2'],
+            'package': [
+                'twine',
+                'wheel',
+            ],
+        },
+
+        test_suite='resolwe_bio.tests',
+
         classifiers=[
             'Development Status :: 4 - Beta',
+
             'Environment :: Web Environment',
             'Framework :: Django',
             'Intended Audience :: Developers',
+            'Topic :: Scientific/Engineering :: Bio-Informatics',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+
             'License :: OSI Approved :: Apache Software License',
+
             'Operating System :: OS Independent',
+
             'Programming Language :: Python',
             'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
-            'Topic :: Scientific/Engineering :: Bio-Informatics',
-            'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-        zip_safe=False,
-        dependency_links=(
-            "git+https://github.com/dblenkus/resolwe.git@5f10f2e7f16f68840de212a9aa1329e5c00fd942#egg=resolwe-0.9.1",
-        ),
-        install_requires=(
-            "resolwe>=0.9.1",
-        ),
-        test_suite='resolwe_bio.tests',
+
+        keywords='bioinformatics resolwe bio pipelines dataflow django',
     )
