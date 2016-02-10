f5c39b6ee82c959f49c1a4f287ef614373410366
Janez Kokosar <janez@genialis.com>
2016-02-10 17:08:24 -0600

    Added additional options to Abyss

--- resolwe_bio/tests/test_assembler.py
+++ resolwe_bio/tests/test_assembler.py
@@ -9,10 +9,17 @@ class AbyssProcessorTestCase(BioProcessTestCase):
         inputs = {'src1': 'reads_paired_abyss_1.fastq.gz', 'src2': 'reads_paired_abyss_2.fastq.gz'}
         reads = self.run_processor('import:upload:reads-fastq-paired-end', inputs)
 
-        inputs = {'reads': reads.pk,
+        inputs = {'paired_end': reads.pk,
                   'se': se_reads.pk,
                   'options': {'k': 25,
                               'name': 'ecoli'}}
 
         self.run_processor('assembler:abyss', inputs)
-        # self.assertFiles(abyss, 'contigs', 'contigs.zip', compression='zip')
+
+        inputs = {'paired_end': reads.pk,
+                  'se': se_reads.pk,
+                  'use_unmapped': True,
+                  'options': {'k': 25,
+                              'name': 'ecoli'}}
+
+        self.run_processor('assembler:abyss', inputs, 'error')
