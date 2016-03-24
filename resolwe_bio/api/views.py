afc2b3f1f2d56c1a4a5e55049350f4950c14ac2d
Domen Blenkuš <domen@blenkus.com>
2016-03-24 11:06:20 +0100

    Add api endpoint for Sample.

---
+++ resolwe_bio/api/views.py
@@ -0,0 +1,29 @@
+from django.db.models import Q
+
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
+
+from resolwe.flow.models import Collection
+from resolwe.flow.views import CollectionViewSet
+
+
+class SampleViewSet(CollectionViewSet):
+    queryset = Collection.objects.filter(descriptor_schema__slug='sample').prefetch_related('descriptor_schema')
+
+    @list_route(methods=[u'get'])
+    def annotated(self, request):
+        """Return list of annotated `Samples`."""
+        queryset = self.get_queryset().filter(
+            Q(descriptor__has_key='geo_annotation') & Q(descriptor__geo_annotation__has_key='annotator'))
+        serializer = self.serializer_class(queryset, many=True)
+
+        return Response(serializer.data)
+
+    @list_route(methods=[u'get'])
+    def unannotated(self, request):
+        """Return list of unannotated `Samples`."""
+        queryset = self.get_queryset().filter(
+            ~Q(descriptor__has_key='geo_annotation') | ~Q(descriptor__geo_annotation__has_key='annotator'))
+        serializer = self.serializer_class(queryset, many=True)
+
+        return Response(serializer.data)
