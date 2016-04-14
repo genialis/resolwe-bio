9ad85a6986c8350cb082f23d9508301c40ca440d
Domen Blenkuš <domen@blenkus.com>
2016-04-14 12:57:00 +0200

    Order samples by date created of the newest Data

--- resolwe_bio/api/views.py
+++ resolwe_bio/api/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Q
+from django.db.models import Max, Q
 
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
@@ -13,8 +13,11 @@ class SampleViewSet(CollectionViewSet):
     @list_route(methods=[u'get'])
     def annotated(self, request):
         """Return list of annotated `Samples`."""
-        queryset = self.get_queryset().filter(
-            Q(descriptor__has_key='geo') & Q(descriptor__geo__has_key='annotator'))
+        queryset = self.get_queryset().annotate(
+            latest_date=Max('data__created')
+        ).filter(
+            Q(descriptor__has_key='geo') & Q(descriptor__geo__has_key='annotator')
+        ).order_by('-latest_date')
         serializer = self.serializer_class(queryset, many=True, context={'request': request})
 
         return Response(serializer.data)
@@ -22,8 +25,11 @@ class SampleViewSet(CollectionViewSet):
     @list_route(methods=[u'get'])
     def unannotated(self, request):
         """Return list of unannotated `Samples`."""
-        queryset = self.get_queryset().filter(
-            ~Q(descriptor__has_key='geo') | ~Q(descriptor__geo__has_key='annotator'))
+        queryset = self.get_queryset().annotate(
+            latest_date=Max('data__created')
+        ).filter(
+            ~Q(descriptor__has_key='geo') | ~Q(descriptor__geo__has_key='annotator')
+        ).order_by('-latest_date')
         serializer = self.serializer_class(queryset, many=True, context={'request': request})
 
         return Response(serializer.data)
