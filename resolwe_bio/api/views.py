e900afc213abe4ceab26eb7cb7aabcb64af9ce8b
Domen Blenkuš <domen@blenkus.com>
2016-04-15 11:42:31 +0200

    Change sample viewset to filter based on query params

--- resolwe_bio/api/views.py
+++ resolwe_bio/api/views.py
@@ -1,35 +1,25 @@
-from django.db.models import Max, Q
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-from rest_framework.decorators import list_route
-from rest_framework.response import Response
+from django.db.models import Max, Q
 
 from resolwe.flow.models import Collection
 from resolwe.flow.views import CollectionViewSet
 
 
 class SampleViewSet(CollectionViewSet):
-    queryset = Collection.objects.filter(descriptor_schema__slug='sample').prefetch_related('descriptor_schema')
 
-    @list_route(methods=[u'get'])
-    def annotated(self, request):
-        """Return list of annotated `Samples`."""
-        queryset = self.get_queryset().annotate(
+    def get_queryset(self):
+        queryset = Collection.objects.annotate(
             latest_date=Max('data__created')
         ).filter(
-            Q(descriptor__has_key='geo') & Q(descriptor__geo__has_key='annotator')
-        ).order_by('-latest_date')
-        serializer = self.serializer_class(queryset, many=True, context={'request': request})
+            descriptor_schema__slug='sample'
+        ).prefetch_related('descriptor_schema')
 
-        return Response(serializer.data)
+        annotated = self.request.query_params.get('annotated', None)
 
-    @list_route(methods=[u'get'])
-    def unannotated(self, request):
-        """Return list of unannotated `Samples`."""
-        queryset = self.get_queryset().annotate(
-            latest_date=Max('data__created')
-        ).filter(
-            ~Q(descriptor__has_key='geo') | ~Q(descriptor__geo__has_key='annotator')
-        ).order_by('-latest_date')
-        serializer = self.serializer_class(queryset, many=True, context={'request': request})
+        if annotated == "1":
+            queryset = queryset.filter(Q(descriptor__has_key='geo') & Q(descriptor__geo__has_key='annotator'))
+        elif annotated == "0":
+            queryset = queryset.filter(~Q(descriptor__has_key='geo') | ~Q(descriptor__geo__has_key='annotator'))
 
-        return Response(serializer.data)
+        return queryset.order_by('-latest_date')
