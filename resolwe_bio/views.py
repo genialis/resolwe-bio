12f83f2a224581192177360255b1e5ed05b6616a
Miha Stajdohar <miha.stajdohar@gmail.com>
2016-05-22 08:02:05 -0500

    Filter only GET requests

--- resolwe_bio/views.py
+++ resolwe_bio/views.py
@@ -22,13 +22,14 @@ class SampleViewSet(CollectionViewSet):
             latest_date=Max('data__created')
         ).prefetch_related('descriptor_schema')
 
-        annotated = self.request.query_params.get('annotated', None)
+        # Return annotated samples only (default for GET requests)
+        if not self.request.data:
+            annotated = self.request.query_params.get('annotated', None)
 
-        # Return annotated samples only by default
-        if annotated == "0":
-            queryset = queryset.filter(~Q(descriptor__has_key='geo') | ~Q(descriptor__geo__has_key='annotator'))
-        else:
-            queryset = queryset.filter(Q(descriptor__has_key='geo') & Q(descriptor__geo__has_key='annotator'))
+            if annotated == "0":
+                queryset = queryset.filter(~Q(descriptor__has_key='geo') | ~Q(descriptor__geo__has_key='annotator'))
+            else:
+                queryset = queryset.filter(Q(descriptor__has_key='geo') & Q(descriptor__geo__has_key='annotator'))
 
         return queryset.order_by('-latest_date')
 
