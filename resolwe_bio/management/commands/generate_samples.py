7665d3319dc6c4f9333aa11cfca3419a64a12973
hadalin <hadalin.primoz@gmail.com>
2016-05-27 10:51:54 +0200

    Update samples generation
    Due to updated models

--- resolwe_bio/management/commands/generate_samples.py
+++ resolwe_bio/management/commands/generate_samples.py
@@ -5,10 +5,12 @@ import random
 import shutil
 import string
 import zipfile
+import datetime
 
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.utils import timezone
 from resolwe.flow.models import Data, DescriptorSchema, Process, Storage
 from resolwe_bio.models import Sample
 from resolwe_bio.tools.utils import escape_mongokey, gzopen
@@ -21,15 +23,15 @@ class Command(BaseCommand):
     help = "Generate test data"
 
     def add_arguments(self, parser):
-        parser.add_argument('-a', '--n-annotated', type=int, default=15,
-                            help="Number of annotated samples to generate (default: %(default)s)")
-        parser.add_argument('-u', '--n-unannotated', type=int, default=5,
-                            help="Number of unannotated samples to generate (default: %(default)s)")
+        parser.add_argument('-s', '--n-samples', type=int, default=15,
+                            help="Number of samples to generate (default: %(default)s)")
+        parser.add_argument('-p', '--n-presamples', type=int, default=5,
+                            help="Number of presamples to generate (default: %(default)s)")
         parser.add_argument('--rseed', action='store_true', help="Use fixed random seed")
 
     @staticmethod
     def get_random_word(length):
-        return ''.join(random.choice(string.lowercase) for i in range(length))
+        return ''.join(random.choice(string.lowercase) for _ in range(length))
 
     def set_name(self):
         organism = random.choice(['Dictyostelium discoideum', 'Mus musculus', 'Homo sapiens'])
@@ -51,7 +53,8 @@ class Command(BaseCommand):
             exit(1)
         return users.first()
 
-    def generate_expressions(self, gene_ids, path):
+    @staticmethod
+    def generate_expressions(gene_ids, path):
         """Generate random expression data"""
         genes = {}
         with gzip.open(os.path.join(path, 'expressions.tab.gz'), 'wb') as f:
@@ -67,15 +70,21 @@ class Command(BaseCommand):
             js_out = '{{"exp_json":{}}}'.format(json.dumps({'genes': genes}, separators=(',', ':')))
             json_file.write(js_out)
 
-    def generate_reads_descriptor(self):
+    @staticmethod
+    def generate_reads_descriptor():
         barcodes = ['ATGCATGC', 'TACGTACG']
         instruments = ['Illumina HiSeq X', 'Illumina HiSeq 3000', 'Illumina HiSeq 3000']
         descriptor = {'barcode': random.choice(barcodes),
                       'instrument_type': random.choice(instruments),
                       'barcode_removed': True}
+
+        seq_date = datetime.date.today().strftime('%Y-%m-%d')
+        descriptor['seq_date'] = seq_date
+
         return descriptor
 
-    def generate_sample_desciptor(self, organism_name):
+    @staticmethod
+    def generate_sample_desciptor(organism_name):
         annotator = ['Billie Joe Armstrong', 'Dexter Holland', 'Mark Hoppus']
         growth_protocol = ('One vial of cells was thawed out and placed in a T75 '
                            'flask in DMEM/F12 supplemented with 10% FBS and fed '
@@ -146,16 +155,19 @@ class Command(BaseCommand):
         human_genes = os.path.join(test_files_path, 'human_genes.tab.gz')
         mouse_genes = os.path.join(test_files_path, 'mouse_genes.tab.gz')
 
-        # crate reads data object
+        # Create reads data object
+        started = timezone.now()
         d = Data.objects.create(
-            name = self.set_name(),
-            descriptor_schema = DescriptorSchema.objects.get(slug='reads'),
-            descriptor = self.generate_reads_descriptor(),
-            status = 'OK',
-            process = Process.objects.get(slug='upload-fastq-single'),
-            contributor = self.set_user(),
-            input = {'src': {'file': os.path.basename(reads)}},
-            output = {
+            name=self.set_name(),
+            started=started,
+            finished=started + datetime.timedelta(minutes=45),
+            descriptor_schema=DescriptorSchema.objects.get(slug='reads'),
+            descriptor=self.generate_reads_descriptor(),
+            status='OK',
+            process=Process.objects.get(slug='upload-fastq-single'),
+            contributor=self.set_user(),
+            input={'src': {'file': os.path.basename(reads)}},
+            output={
                 'fastq': {'file': os.path.basename(reads)},
                 'fastqc_url': {
                     'url': 'fastqc/{}_fastqc/fastqc_report.html'.format(reads_name),
@@ -188,14 +200,14 @@ class Command(BaseCommand):
 
         # Upload bam file
         bam = Data.objects.create(
-            name = 'Mapping',
-            process = Process.objects.get(slug='upload-bam-indexed'),
-            contributor = self.set_user(),
-            status = 'OK',
-            input = {
+            name='Mapping',
+            process=Process.objects.get(slug='upload-bam-indexed'),
+            contributor=self.set_user(),
+            status='OK',
+            input={
                 'src': {'file': 'alignment_position_sorted.bam'},
                 'src2': {'file': 'alignment_position_sorted.bam.bai'}},
-            output = {
+            output={
                 'bam': {'file': 'alignment_position_sorted.bam'},
                 'bai': {'file': 'alignment_position_sorted.bam.bai'}})
 
@@ -212,30 +224,31 @@ class Command(BaseCommand):
 
         # Create expressios
         exp = Data.objects.create(
-            name = 'Expression',
-            process = Process.objects.get(slug='upload-expression'),
-            contributor = self.set_user(),
-            status = 'OK',
-            input = {'exp': {'file': 'expressions.tab.gz'}, 'exp_type': 'FPKM'})
+            name='Expression',
+            process=Process.objects.get(slug='upload-expression'),
+            contributor=self.set_user(),
+            status='OK',
+            input={'exp': {'file': 'expressions.tab.gz'}, 'exp_type': 'FPKM'})
 
         os.mkdir(os.path.join(data_dir, str(exp.id)))
 
         if d.name.startswith('Dd'):
-            self.generate_expressions(dicty_genes, os.path.join(data_dir, str(exp.id))
+            self.generate_expressions(dicty_genes, os.path.join(data_dir, str(exp.id)))
         if d.name.startswith('Hs'):
             self.generate_expressions(human_genes, os.path.join(data_dir, str(exp.id)))
         if d.name.startswith('Mm'):
             self.generate_expressions(mouse_genes, os.path.join(data_dir, str(exp.id)))
 
         json_object = Storage.objects.create(
-            json = json.load(open(os.path.join(data_dir, str(exp.id), 'expressions.json'))),
-            contributor = self.set_user(),
-            data = exp)
+            json=json.load(open(os.path.join(data_dir, str(exp.id), 'expressions.json'))),
+            contributor=self.set_user(),
+            data=exp)
 
         exp.output = {
                 'exp': {'file': 'expressions.tab.gz'},
                 'exp_type': 'FPKM',
-                'exp_json': json_object.id}
+                'exp_json': json_object.id
+        }
 
         exp.save()
 
@@ -249,16 +262,16 @@ class Command(BaseCommand):
         # Annotate Sample Collection
         if annotated:
             sample.descriptor = self.generate_sample_desciptor(d.name)
+            sample.presample = False
             sample.save()
-            print "Created annotated sample: {} (id={})".format(sample.name, sample.id)
+            print "Created sample: {} (id={})".format(sample.name, sample.id)
         else:
-            print "Created unannotated sample: {} (id={})".format(sample.name, sample.id)
+            print "Created presample: {} (id={})".format(sample.name, sample.id)
 
     def handle(self, *args, **options):
         if options['rseed']:
             random.seed(42)
-        for i in range(options['n_annotated']):
+        for i in range(options['n_samples']):
             self.create_data(annotated=True)
-        for i in range(options['n_unannotated']):
+        for i in range(options['n_presamples']):
             self.create_data(annotated=False)
-
