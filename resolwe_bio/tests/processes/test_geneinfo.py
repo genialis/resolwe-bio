88a1be7bd45791b65d0a89a4ed7bcfb8f23f7882
Janez Kokosar <janez@genialis.com>
2016-07-25 10:31:23 -0500

    Update upload-geneinfo processor

--- resolwe_bio/tests/processes/test_geneinfo.py
+++ resolwe_bio/tests/processes/test_geneinfo.py
@@ -4,7 +4,6 @@ from resolwe_bio.utils.test import skipDockerFailure, BioProcessTestCase
 
 class GIProcessorTestCase(BioProcessTestCase):
 
-    @skipDockerFailure("Fails with: ImportError: No module named xlrd")
     def test_gi(self):
         inputs = {'src': 'mouse_gene_info.txt'}
         self.run_processor("upload-geneinfo", inputs)
