06b9881b0fcf1c34dc60bb64b39d0be4b2240cbc
Janez Kokosar <janez@genialis.com>
2017-03-07 21:51:25 +0100

    Refactor amplicon report generator
    Add analysis date and panel name
    Add section numbering
    Remove barchart
    Improve QA statistics reports
    Add varinats tables

--- resolwe_bio/tests/processes/test_generate_report.py
+++ resolwe_bio/tests/processes/test_generate_report.py
@@ -1,42 +1,43 @@
 # pylint: disable=missing-docstring
-from resolwe_bio.utils.test import skipDockerFailure, BioProcessTestCase
+from resolwe_bio.utils.test import BioProcessTestCase
 
 
 class ReportProcessorTestCase(BioProcessTestCase):
 
-    @skipDockerFailure("Processor requires a custom Docker image.")
     def test_amplicon_report(self):
         template = self.run_process('upload-file', {'src': 'report_template.tex'})
         logo = self.run_process('upload-file', {'src': 'genialis_logo.pdf'})
 
-        bam = self.run_process('upload-bam', {'src': '56GSID_10k_trimmed.bam'})
-        bed = self.run_process('upload-bed', {'src': '56g_targets_small.bed'})
-
-        coverage = self.run_process('coveragebed', {'alignment': bam.id, 'bed': bed.id})
-
-        genome = self.run_process('upload-genome', {'src': 'hs_b37_chr22_frag.fasta.gz'})
-        bed_picard = self.run_process('upload-bed', {'src': '56g_targets_picard_small.bed'})
-
-        inputs = {'src': 'Mills_and_1000G_gold_standard.indels.b37.chr22_small.vcf.gz'}
-        indels = self.run_process('upload-variants-vcf', inputs)
-
-        dbsnp = self.run_process('upload-variants-vcf', {'src': 'dbsnp_138.b37.chr22_small.vcf.gz'})
-
         inputs = {
-            'alignment': bam.id,
-            'bed': bed_picard.id,
-            'genome': genome.id,
-            'known_indels': [indels.id],
-            'known_vars': [dbsnp.id]
+            'src': '56GSID_10k_trimmed.bam.realigned.bqsrCal.bam',
+            'target_pcr_metrics': '56GSID_10k_trimmed.bam.targetPCRmetrics.txt',
+            'target_coverage': '56GSID_10k_trimmed.bam.perTargetCov.txt'
         }
 
-        preprocess_bam = self.run_process('vc-preprocess-bam', inputs)
+        preprocess_bam = self.run_process('upload-bam-vc', inputs)
+
+        inputs = {
+            'cov': '56GSID_10k_trimmed.cov',
+            'covd': '56GSID_10k_trimmed.covd'
+        }
+
+        coverage = self.run_process('upload-coverage', inputs)
+
+        inputs = {
+            'annotation': '56GSID.lf.finalvars.txt',
+            'summary': '56GSID_1k.gatkHC_snpEff_summary.html',
+            'snpeff_genes': '56GSID_1k.gatkHC_snpEff_genes.txt'
+        }
+
+        annot_variants = self.run_process('upload-snpeff', inputs)
 
         report_inputs = {
             'bam': preprocess_bam.id,
             'coverage': coverage.id,
             'template': template.id,
-            'logo': logo.id
+            'logo': logo.id,
+            'panel_name': '56G Oncology Panel v2',
+            'annot_vars': [annot_variants.id]
         }
 
         self.run_process('amplicon-report', report_inputs)
