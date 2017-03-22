29d8f4229310282b311107bfc1a96220d4699f1a
Barbara Jenko <barbara.jenko@gmail.com>
2017-03-22 20:59:51 +0100

    Add workflow bbduk-star-htseq

--- resolwe_bio/tests/workflows/test_rna_seq.py
+++ resolwe_bio/tests/workflows/test_rna_seq.py
@@ -1,5 +1,6 @@
 # pylint: disable=missing-docstring
 from resolwe_bio.utils.test import BioProcessTestCase
+from resolwe.flow.models import Data
 
 
 class RNASeqWorkflowTestCase(BioProcessTestCase):
@@ -17,3 +18,32 @@ class RNASeqWorkflowTestCase(BioProcessTestCase):
                 'annotation': annotation.id,
             }
         )
+
+    def test_bbduk_star_htseq_workflow(self):
+        inputs = {'src': ['workflow_bbduk_star_test.fastq.gz']}
+        reads = self.run_processor('upload-fastq-single', inputs)
+
+        inputs = {'src': 'HS_chr21_ensemble.fa.gz'}
+        star_index_fasta = self.run_process('upload-fasta-nucl', inputs)
+
+        inputs = {'src': 'poly_A.fa.gz'}
+        adapters1 = self.run_process('upload-fasta-nucl', inputs)
+
+        inputs = {'src': 'HS_chr21_short.gtf.gz', 'source': 'ENSEMBLE'}
+        annotation = self.run_process('upload-gtf', inputs)
+
+        inputs = {'annotation': annotation.id, 'genome2': star_index_fasta.id}
+        star_index = self.run_process('alignment-star-index', inputs)
+
+        self.run_process(
+            'workflow-bbduk_star_htseq', {
+                'reads': reads.id,
+                'star_index': star_index.id,
+                'bbduk_adapters': [adapters1.id],
+                'annotation': annotation.id,
+                'stranded': 'yes'
+            }
+        )
+
+        workflow = Data.objects.last()
+        self.assertFile(workflow, 'rc', 'workflow_reads_rc.tab.gz', compression='gzip')
