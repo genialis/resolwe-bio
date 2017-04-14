6a7af7c8fe06ced72071299f827bb9a4a6df1f97
Barbara Jenko <barbara.jenko@gmail.com>
2017-04-14 11:05:32 +0200

    Recipes page for BCM and chip-seq workflow

---
+++ resolwe_bio/tests/workflows/test_chip_seq.py
@@ -0,0 +1,21 @@
+# pylint: disable=missing-docstring
+from resolwe_bio.utils.test import BioProcessTestCase
+
+
+class ChipSeqWorkflowTestCase(BioProcessTestCase):
+    def test_chip_seq_workflow(self):
+        genome = self.prepare_genome()
+        reads = self.prepare_reads()
+
+        inputs = {'src': 'annotation.gff.gz', 'source': 'DICTYBASE'}
+        annotation = self.run_process('upload-gff3', inputs)
+
+        self.run_process(
+            'workflow-chip-seq', {
+                'reads': reads.id,
+                'genome': genome.id,
+                'annotation': annotation.id,
+                'macs_gsize': 'dm',
+                'rose_genome': 'MM9'
+            }
+        )
