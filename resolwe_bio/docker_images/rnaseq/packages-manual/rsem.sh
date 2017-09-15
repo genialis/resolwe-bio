8581dc7104c6fe3ae0e6e82a9a4a1a08c56b4508
Matjaž Žganec <matjaz@genialis.com>
2017-09-15 15:29:44 +0200

    Refactor 'resolwebio/rnaseq' docker image
    Use the same structure as 'resolwebio/{base,legacy}' docker images.

---
+++ resolwe_bio/docker_images/rnaseq/packages-manual/rsem.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+. /var/cache/build/packages-manual/common.sh
+
+download_and_verify \
+    deweylab \
+    rsem \
+    1.3.0 \
+    a6776aae25a690375504974d65e8554d6e3ddab93cef5b82969064854d22c09f \
+    https://github.com/deweylab/RSEM/archive/v\${version}.tar.gz \
+    RSEM-\${version}
+
+make
+make install
+
+add_binary_path \
+    deweylab \
+    rsem
