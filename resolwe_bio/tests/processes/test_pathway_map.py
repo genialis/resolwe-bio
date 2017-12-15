f620d9b8c277f187eee9fd6180347e53de6129ec
Mátyás Fodor <matyas.fodor@gmail.com>
2017-12-15 14:19:23 +0100

    Extend pathway map with species and source field

--- resolwe_bio/tests/processes/test_pathway_map.py
+++ resolwe_bio/tests/processes/test_pathway_map.py
@@ -9,6 +9,12 @@ class PathwayMapTestCase(BioProcessTestCase):
     @tag_process('upload-metabolic-pathway')
     def test_upload_metabolic_pathway(self):
         file_name = 'matabolic pathway.json.gz'
-        inputs = {'src': file_name}
+        inputs = {
+            'src': file_name,
+            'species': 'Homo Sapiens',
+            'source': 'BIGG',
+        }
         upload = self.run_process("upload-metabolic-pathway", inputs)
         self.assertJSON(upload, upload.output['pathway'], '', file_name)
+        self.assertEqual(upload.output['species'], 'Homo Sapiens')
+        self.assertEqual(upload.output['source'], 'BIGG')
