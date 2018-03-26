5ab4bbdfb4478bac443f1d6d1d31c197981d310d
Barbara Jenko <barbara@genialis.com>
2018-03-26 10:41:25 +0200

    Use build information in IGV

--- resolwe_bio/tools/make_igv_session_archive.py
+++ resolwe_bio/tools/make_igv_session_archive.py
@@ -8,28 +8,48 @@ from lxml import etree
 
 parser = argparse.ArgumentParser(description="Create igv session.")
 
-parser.add_argument('-f', '--input_file', required=True, help="File with paths to files for IGV")
+parser.add_argument('-f', '--input_file', required=True, help="File with paths to files for IGV.")
 
 args = parser.parse_args()
+
+
+def get_build_info(input_file):
+    """Get build information in UCSC notation, if possible."""
+    build_dict = {
+        'hg38': ['GRCh38'],
+        'hg19': ['GRCh37', 'b37'],
+        'mm10': ['GRCm38'],
+        'mm9': ['MGSCv37'],
+        'rn6': ['Rnor_6.0'], }
+
+    _, build, _, _ = input_file.split('_')
+    new_build = [k for k, v in build_dict.items() if k == build or build.startswith(tuple(v))]
+    if new_build:
+        return new_build[0]
+    return ''
 
 
 def make_xml_tree(input_file):
     """Make xml tree for IGV session."""
     global_ = etree.Element(  # pylint: disable=no-member
         'Global',
+        genome=get_build_info(input_file),
         version='3',
     )
 
     resources = etree.SubElement(global_, 'Resources')  # pylint: disable=no-member
 
     with open(input_file, 'r') as tfile:
-        for line in tfile:
-            line = line.rstrip()
+        for filename in tfile:
+            filename = filename.rstrip()
+            #  replace None (dir folder if species and build are not defined) with other_data
+            if os.path.dirname(os.path.dirname(filename)) == 'None':
+                filename = filename.replace('None', 'other_data')
             etree.SubElement(  # pylint: disable=no-member
                 resources,
                 'Resource',
-                name=os.path.basename(line),
-                path=os.path.join('..', line),
+                name=os.path.basename(filename),
+                path=os.path.join('..', filename),
             )
 
     doc = etree.tostring(  # pylint: disable=no-member
@@ -43,7 +63,9 @@ def make_xml_tree(input_file):
 
 def write_xml_file(input_file, doc):
     """Compose a name and write output file."""
-    with open(input_file.replace('temp_igv.txt', 'igv.xml'), 'wb') as f:
+    os.makedirs('IGV', exist_ok=True)
+    output_name = os.path.join('IGV', input_file.replace('temp_igv.txt', 'igv.xml'))
+    with open(output_name, 'wb') as f:
         f.write(doc)
 
 
