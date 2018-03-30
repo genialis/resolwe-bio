177d8defaf3e260ce2d7f243a774617ac9566513
Jernej Kos <jernej@kos.mx>
2018-03-30 14:29:18 +0200

    Add resolwe-bio extensions of Resolwe core

---
+++ resolwe_bio/extensions.py
@@ -0,0 +1,45 @@
+"""Resolwe-bio extensions of Resolwe core."""
+import elasticsearch_dsl as dsl
+from elasticsearch_dsl.query import Q
+
+from resolwe.elastic.composer import composer
+
+
+class ExtendedDataDocument(object):
+    """Data ES document extensions."""
+
+    source = dsl.Keyword()
+    species = dsl.Text()
+    build = dsl.Keyword()
+    feature_type = dsl.Keyword()
+
+
+class ExtendedDataIndex(object):
+    """Data ES index extensions."""
+
+    mapping = {
+        'source': 'output.source',
+        'species': 'output.species',
+        'build': 'output.build',
+        'feature_type': 'output.feature_type',
+    }
+
+
+class ExtendedDataViewSet(object):
+    """Data viewset extensions."""
+
+    filtering_fields = ('source', 'species', 'build', 'feature_type')
+
+    def text_filter(self, value):
+        """Extend full-text data filter."""
+        return [
+            Q('match', species={'query': value, 'operator': 'and', 'boost': 2.0}),
+            Q('match', source={'query': value, 'operator': 'and', 'boost': 2.0}),
+            Q('match', build={'query': value, 'operator': 'and', 'boost': 2.0}),
+            Q('match', feature_type={'query': value, 'operator': 'and', 'boost': 1.0}),
+        ]
+
+
+composer.add_extension('resolwe.flow.elastic_indexes.data.DataDocument', ExtendedDataDocument)
+composer.add_extension('resolwe.flow.elastic_indexes.data.DataIndex', ExtendedDataIndex)
+composer.add_extension('resolwe.flow.views.data.DataViewSet', ExtendedDataViewSet)
---
+++ resolwe_bio/tests/unit/test_api.py
@@ -0,0 +1,89 @@
+# pylint: disable=missing-docstring
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate
+
+from resolwe.flow.models import Data, DescriptorSchema, Process
+from resolwe.flow.views import DataViewSet
+from resolwe.test import TestCase
+
+factory = APIRequestFactory()  # pylint: disable=invalid-name
+
+
+class TestDataViewSetFilters(TestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.data_viewset = DataViewSet.as_view(actions={
+            'get': 'list',
+        })
+
+        self.proc = Process.objects.create(
+            type='data:test:process',
+            slug='test-process',
+            version='1.0.0',
+            contributor=self.contributor,
+            flow_collection='test-schema',
+            input_schema=[{'name': 'input_data', 'type': 'data:test:', 'required': False}],
+            output_schema=[
+                {'name': 'source', 'type': 'basic:string:'},
+                {'name': 'species', 'type': 'basic:string:'},
+                {'name': 'build', 'type': 'basic:string:'},
+                {'name': 'feature_type', 'type': 'basic:string:'},
+            ]
+        )
+
+        self.descriptor_schema = DescriptorSchema.objects.create(
+            slug='test-schema',
+            version='1.0.0',
+            contributor=self.contributor,
+        )
+
+        self.data = []
+        for index in range(10):
+            data = Data.objects.create(
+                name='Data {}'.format(index),
+                contributor=self.contributor,
+                process=self.proc,
+                status=Data.STATUS_DONE,
+                output={
+                    'source': 'NCBI{}'.format(index),
+                    'species': 'Mus musculus' if index < 5 else 'Homo sapiens',
+                    'build': 'B{}'.format(index),
+                    'feature_type': 'gene' if index < 5 else 'foo',
+                }
+            )
+
+            self.data.append(data)
+
+    def _check_filter(self, query_args, expected):
+        request = factory.get('/', query_args, format='json')
+        force_authenticate(request, self.admin)
+        response = self.data_viewset(request)
+        expected = [item.pk for item in expected]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), len(expected))
+        for item in response.data:
+            self.assertIn(item['id'], expected)
+
+    def test_filter_source(self):
+        self._check_filter({'source': 'NCBI1'}, [self.data[1]])
+        self._check_filter({'source': 'NCBI5'}, [self.data[5]])
+        self._check_filter({'source': 'UCSC'}, [])
+
+    def test_filter_species(self):
+        self._check_filter({'species': 'Mus musculus'}, self.data[:5])
+        self._check_filter({'species': 'Mus'}, self.data[:5])
+        self._check_filter({'species': 'musculus'}, self.data[:5])
+        self._check_filter({'species': 'Homo sapiens'}, self.data[5:])
+        self._check_filter({'species': 'Homo'}, self.data[5:])
+        self._check_filter({'species': 'sapiens'}, self.data[5:])
+
+    def test_filter_build(self):
+        self._check_filter({'build': 'B1'}, [self.data[1]])
+        self._check_filter({'build': 'B5'}, [self.data[5]])
+        self._check_filter({'build': 'XXX'}, [])
+
+    def test_filter_feature_type(self):
+        self._check_filter({'feature_type': 'gene'}, self.data[:5])
+        self._check_filter({'feature_type': 'foo'}, self.data[5:])
