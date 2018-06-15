0604a18f3de5b1f943b28cf5fd0f169027707d2f
Barbara Jenko <barbara@genialis.com>
2018-06-15 10:05:54 +0200

    Fix wigToBigWig and bed_file_corrections in MACS14

--- resolwe_bio/tools/bed_file_corrections_genome_browsers.py
+++ resolwe_bio/tools/bed_file_corrections_genome_browsers.py
@@ -10,6 +10,6 @@ args = parser.parse_args()
 
 df = pd.read_csv(args.bed_file, delimiter='\t', header=None, dtype=str)
 df.iloc[:, 4] = pd.to_numeric(df.iloc[:, 4]).round().astype(int)
-df.iloc[:, 4].clip(upper=1000)
+df.iloc[:, 4] = df.iloc[:, 4].clip(upper=1000)
 output_name = '_'.join(['corrected', args.bed_file])
 df.to_csv(output_name, sep='\t', index=False, header=False)
