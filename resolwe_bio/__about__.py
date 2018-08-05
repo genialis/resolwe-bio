e907c7de622c54556df10155caddbb05c8235d19
Miha Stajdohar <miha.stajdohar@gmail.com>
2018-08-06 00:39:01 +0200

    Change author in about file

--- resolwe_bio/__about__.py
+++ resolwe_bio/__about__.py
@@ -10,7 +10,7 @@ __url__ = 'https://github.com/genialis/resolwe-bio'
 # https://packaging.python.org/en/latest/distributing/#semantic-versioning-preferred
 __version__ = '12.0.0a2'
 
-__author__ = 'Genialis d.o.o.'
+__author__ = 'Genialis, Inc.'
 __email__ = 'dev-team@genialis.com'
 
 __license__ = 'Apache License (2.0)'
