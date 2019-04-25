34aa46e4d5f1ab4a66a5b8279502a8bc6954d683
Janez Kokosar <janez@genialis.com>
2019-04-25 11:36:38 +0200

    Pin urllib to 1.24.x

--- setup.py
+++ setup.py
@@ -46,6 +46,9 @@ setuptools.setup(
         'elasticsearch-dsl~=5.4.0',
         # XXX: Required due to issue https://github.com/pypa/pip/issues/4905.
         'resolwe >=16.0a1, ==16.*',
+        # XXX: Temporarily pin urllib to 1.24.x, since requests 2.21.0
+        # has requirement urllib3<1.25,>=1.21.1
+        'urllib3~=1.24.2',
         'wrapt>=1.10.8',
         'django-filter~=2.0.0',
     ),
