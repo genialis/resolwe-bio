f0b523aa8ade7761e775aaa5a6bce4b23373f3e1
Janez Kokosar <janez@genialis.com>
2019-12-10 11:27:23 +0100

    Bump memory requirements in slam-count

--- resolwe_bio/processes/slamdunk/slam_count.py
+++ resolwe_bio/processes/slamdunk/slam_count.py
@@ -42,7 +42,7 @@ class SlamCount(Process):
         },
         'resources': {
             'cores': 1,
-            'memory': 1024,
+            'memory': 4096,
             'network': True,
         },
     }
@@ -51,7 +51,7 @@ class SlamCount(Process):
         'type': 'sample',
     }
     data_name = '{{ tcount|sample_name|default("?") }}'
-    version = '1.0.0'
+    version = '1.0.1'
 
     class Input:
         """Input fields for SlamCount."""
