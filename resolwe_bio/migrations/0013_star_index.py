628f41751f247b7bc7e4a6422d33355bd8913bca
Janez Kokosar <janez@genialis.com>
2020-03-19 14:04:07 +0100

    Migrate STAR index process type

---
+++ resolwe_bio/migrations/0013_star_index.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+from resolwe.flow.migration_ops import ResolweProcessChangeType
+
+
+class Migration(migrations.Migration):
+    """
+    Change the ``alignment-star-index`` process type.
+    """
+
+    dependencies = [
+        ('resolwe_bio', '0012_full_text_search'),
+    ]
+
+    operations = [
+        ResolweProcessChangeType(
+            process='alignment-star-index',
+            new_type='data:index:star:',
+        ),
+    ]
