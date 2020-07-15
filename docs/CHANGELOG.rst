891dfdbf25696bfbc538a424cc351fe0acf6e57e
Gregor Jerše <gregor.jerse@genialis.com>
2020-07-15 09:39:10 +0200

    Bump Resolwe version to 23.x.

--- docs/CHANGELOG.rst
+++ docs/CHANGELOG.rst
@@ -5,6 +5,15 @@ Change Log
 All notable changes to this project are documented in this file.
 This project adheres to `Semantic Versioning <http://semver.org/>`_.
 
+==========
+Unreleased
+==========
+
+Changed
+-------
+- **BACKWARD INCOMPATIBLE:** Require Resolwe 23.x.
+
+
 ===================
 31.0.0 - 2020-07-10
 ===================
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setuptools.setup(
         'djangorestframework~=3.9.0',
         'django-filter~=2.0.0',
         # XXX: Required due to issue https://github.com/pypa/pip/issues/4905.
-        'resolwe >=22.0a1, ==22.*',
+        'resolwe >=23.0a1, ==23.*',
         # XXX: Temporarily pin urllib to 1.24.x, since requests 2.21.0
         # has requirement urllib3<1.25,>=1.21.1
         'urllib3~=1.24.2',
