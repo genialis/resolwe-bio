0ab6567e33f1c831a1f34abc44b923f9f97b3219
Janez Kokosar <janez@genialis.com>
2020-08-06 15:25:49 +0200

    Pin XML R package version Later versions require R >= 4.0.0

--- resolwe_bio/docker_images/rnaseq/packages-manual/r-packages.sh
+++ resolwe_bio/docker_images/rnaseq/packages-manual/r-packages.sh
@@ -6,8 +6,10 @@
 #d XXX: This is unverifiable and thus may compromise the whole image.
 # XXX: Use notary (https://github.com/ropenscilabs/notary) when ready.
 sed -e 's/^#.*$//g' -e '/^$/d' /var/cache/build/packages-r.txt | \
-    Rscript --slave --no-save --no-restore-history \
-        -e "install.packages(readLines('stdin'), repos='https://cloud.r-project.org')"
+    Rscript --slave --no-save --no-restore-history -e " \
+      install.packages('https://cran.r-project.org/src/contrib/Archive/XML/XML_3.99-0.3.tar.gz', repos=NULL, type='source'); \
+      install.packages(readLines('stdin'), repos='https://cloud.r-project.org'); \
+    "
 
 # XXX: This is unverifiable and in WITHOUT HTTPS and thus may compromise the whole image.
 # XXX: Use notary (https://github.com/ropenscilabs/notary) when ready.
