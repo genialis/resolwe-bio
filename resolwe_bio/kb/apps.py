5ad7627b5ed38a41bf682593bd4ac9ed04db659a
Gregor Jerše <gregor.jerse@genialis.com>
2021-01-15 07:41:06 +0100

    Expose knowledgebase tables to listener

--- resolwe_bio/kb/apps.py
+++ resolwe_bio/kb/apps.py
@@ -14,3 +14,10 @@ class KnowledgeBaseConfig(AppConfig):
     name = "resolwe_bio.kb"
     label = "resolwe_bio_kb"
     verbose_name = "Resolwe Bioinformatics Knowledge Base"
+
+    def ready(self):
+        """Perform application initialization."""
+        from resolwe_bio.kb.listener_plugin import (  # noqa: F401
+            ExposeFeature,
+            ExposeMapping,
+        )
---
+++ resolwe_bio/kb/listener_plugin.py
@@ -0,0 +1,27 @@
+"""Register knowledgebase tables in listener."""
+from django.contrib.auth import get_user_model
+from django.db.models import QuerySet
+
+from resolwe.flow.managers.listener.python_process_plugin import ExposeObjectPlugin
+
+UserClass = get_user_model()
+
+
+class ExposeFeature(ExposeObjectPlugin):
+    """Expose the Feature knowledge base model in listener."""
+
+    full_model_name = "resolwe_bio_kb.Feature"
+
+    def filter_objects(self, user: UserClass, queryset: QuerySet) -> QuerySet:
+        """Filter the objects for the given user."""
+        return queryset
+
+
+class ExposeMapping(ExposeObjectPlugin):
+    """Expose the Mapping knowledge base model in listener."""
+
+    full_model_name = "resolwe_bio_kb.Mapping"
+
+    def filter_objects(self, user: UserClass, queryset: QuerySet) -> QuerySet:
+        """Filter the objects for the given user."""
+        return queryset
