192bb9242954160c7aa1ad1d0d6e919c3d004da7
Roman Luštrik <roman.lustrik@biolitika.si>
2022-01-13 20:18:37 +0100

    Pin R package versions in resolwebio/common Docker image
    Because only certain parts of the r-base bundle are installed,
    some packages for openssl, curl and xml need to be added manually.
    BiocManager doesn't need to be installed explicitly as this is
    done automatically by renv package.

--- resolwe_bio/docker_images/common/packages-manual/r-packages.sh
+++ resolwe_bio/docker_images/common/packages-manual/r-packages.sh
@@ -3,17 +3,12 @@
 . /var/cache/build/packages-manual/common.sh
 
 # Install R packages.
-#d XXX: This is unverifiable and thus may compromise the whole image.
-# XXX: Use notary (https://github.com/ropenscilabs/notary) when ready.
+# Install renv which is a pre-requisite to installing all other packages in next chunk.
+Rscript --slave --no-save --no-restore-history -e "install.packages('renv', repos = 'https://cloud.r-project.org')"
+
 sed -e 's/^#.*$//g' -e '/^$/d' /var/cache/build/packages-r.txt | \
     Rscript --slave --no-save --no-restore-history \
-        -e "install.packages(readLines('stdin'), repos='https://cloud.r-project.org')"
-
-# XXX: This is unverifiable and in WITHOUT HTTPS and thus may compromise the whole image.
-# XXX: Use notary (https://github.com/ropenscilabs/notary) when ready.
-sed -e 's/^#.*$//g' -e '/^$/d' /var/cache/build/packages-r-bioconductor.txt | \
-    Rscript --slave --no-save --no-restore-history \
-        -e "BiocManager::install(readLines('stdin'))"
+        -e "renv::install(readLines('stdin'), repos='https://cloud.r-project.org')"
 
 # XXX: This is unverifiable and thus may compromise the whole image.
 # XXX: Use notary (https://github.com/ropenscilabs/notary) when ready.
