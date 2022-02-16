ba3a09902e24941d2558eac426f513b39c27f108
Roman Luštrik <roman.lustrik@biolitika.si>
2022-02-16 14:34:09 +0100

    Pin R package versions in resolwebio/rnaseq:6.0.0 Docker image

--- resolwe_bio/docker_images/rnaseq/packages-manual/r-packages.sh
+++ resolwe_bio/docker_images/rnaseq/packages-manual/r-packages.sh
@@ -6,22 +6,12 @@
 #d XXX: This is unverifiable and thus may compromise the whole image.
 # XXX: Use notary (https://github.com/ropenscilabs/notary) when ready.
 sed -e 's/^#.*$//g' -e '/^$/d' /var/cache/build/packages-r.txt | \
-    Rscript --slave --no-save --no-restore-history -e " \
-      install.packages('https://cran.r-project.org/src/contrib/Archive/XML/XML_3.99-0.3.tar.gz', repos=NULL, type='source'); \
-      install.packages(readLines('stdin'), repos='https://cloud.r-project.org'); \
-    "
-
-# XXX: This is unverifiable and in WITHOUT HTTPS and thus may compromise the whole image.
-# XXX: Use notary (https://github.com/ropenscilabs/notary) when ready.
-sed -e 's/^#.*$//g' -e '/^$/d' /var/cache/build/packages-r-bioconductor.txt | \
-    Rscript --slave --no-save --no-restore-history \
-        -e "BiocManager::install(readLines('stdin'))"
+    Rscript --slave --no-save --no-restore-history -e "renv::install(readLines('stdin'))"
 
 # XXX: This is unverifiable and thus may compromise the whole image.
 # XXX: Use notary (https://github.com/ropenscilabs/notary) when ready.
 Rscript --slave --no-save --no-restore-history -e " \
-  library(devtools); \
-  install_github('jkokosar/RNASeqT'); \
-  install_github('jvrakor/Subread_to_DEXSeq', subdir = 'loadSubread'); \
-  install_github('genialis/shRNAde'); \
+  devtools::install_github('jkokosar/RNASeqT'); \
+  devtools::install_github('jvrakor/Subread_to_DEXSeq', subdir = 'loadSubread'); \
+  renv::install('genialis/shRNAde'); \
 "
