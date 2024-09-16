d7aa5d08f903e8c483a1383be27b4ef125571cd3
Marcel Levstek <62072754+marcellevstek@users.noreply.github.com>
2024-09-16 15:28:23 +0200

    Replace nan with empty string
    in variant-annotation process

--- resolwe_bio/processes/annotation/variant_annotation.py
+++ resolwe_bio/processes/annotation/variant_annotation.py
@@ -168,7 +168,7 @@ def create_annotation_list(variants_table, canonical_transcripts, genome_assembl
     variants_table = variants_table.astype(str)
     column_names = {name: name.replace(".", "_") for name in variants_table.columns}
     variants_table.columns = [column_names[col] for col in variants_table.columns]
-    variants_table = variants_table.fillna("")
+    variants_table = variants_table.replace("nan", "")
 
     variant_group = variants_table.groupby(
         ["CHROM", "POS", "REF", "ALT", "TYPE", "CLNDN", "CLNSIG", "SNPID", "CLINVARID"]
@@ -233,7 +233,7 @@ class VariantAnnotation(ProcessBio):
         },
     }
     data_name = "Variant annotation"
-    version = "1.0.3"
+    version = "1.0.4"
     process_type = "data:annotation"
     category = "Annotation"
     scheduling_class = SchedulingClass.BATCH
