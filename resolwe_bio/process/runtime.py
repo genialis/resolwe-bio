5ec088a8b2b144dc44ecae54ed876ca8cf34af94
Gregor Jerše <gregor.jerse@genialis.com>
2025-02-24 13:16:20 +0100

    Send variants from processes in batches

--- resolwe_bio/process/runtime.py
+++ resolwe_bio/process/runtime.py
@@ -1,12 +1,13 @@
 """Resolwe-bio process with KnowledgeBase extensions."""
 
+from itertools import batched
 from typing import Any, Dict, List
+
+from resolwe_bio.process.models import Feature, Mapping, Variant, VariantCall
 
 from resolwe.process.communicator import communicator
 from resolwe.process.models import Data
 from resolwe.process.runtime import Process
-
-from resolwe_bio.process.models import Feature, Mapping, Variant, VariantCall
 
 
 class ProcessBio(Process):
@@ -29,4 +30,6 @@ class ProcessBio(Process):
 
     def add_variants_annotations(self, variants: List[Dict[str, Any]]):
         """Add variants annotations to the database."""
-        return communicator.add_variants_annotations(variants)
+        BATCH_SIZE = 1000
+        for batch in batched(variants, BATCH_SIZE):
+            communicator.add_variants_annotations(batch)
